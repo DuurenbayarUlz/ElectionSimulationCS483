dc5547f2b0bff61cdf920be84422c29dd9e24bfd
Duuree Ulziiduuren <duuree@nestsolutions.llc>
2022-03-10 16:37:55 -0500

    firebase

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { db } from "./firebase";
 
 function App() {
+  const [votes, setVotes] = useState([]);
+  const votesCollectionRef = collection(db, "votes");
+
+  useEffect(() => {
+    const getVotes = async () => {
+      const data = await getDocs(votesCollectionRef);
+      setVotes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getVotes();
+  }, [usersCollectionRef]);
+
   return (
     <div className="App">
       <h2>Voting Simulation</h2>
+      {votes.map((vote) => {
+        return <div>{vote}</div>;
+      })}
     </div>
   );
 }
